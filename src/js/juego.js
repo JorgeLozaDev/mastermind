50406b9874e8a88746bef2a0f5cfbe414b702c06
Jorge <j0rg3e93@gmail.com>
2023-10-11 18:54:56 +0200

    Se cambia el tamaño de la image, se empieza ha hacer la logica de la pagina de juegos

--- src/js/juego.js
+++ src/js/juego.js
@@ -2,6 +2,7 @@
 let dificultad = sessionStorage.getItem("dificult");
 let nombreJugador = sessionStorage.getItem("j1");
 let coloresAJugar = JSON.parse(sessionStorage.getItem("coloresJuego"));
+let currentRow = 0;
 
 // buscamos el id donde ira el nombre y lo colocamos en su caja
 document.getElementById("nombre").innerHTML = nombreJugador;
@@ -21,9 +22,33 @@ const pintarTablaSegunDificultad = (dificultad) => {
 };
 
 const pintarTabla = (fila) => {
-  let tabla = document.getElementById("tabla");
-//   tabla.appendChild("tr");
+  let tabla = document.getElementById("tabla").getElementsByTagName("tbody")[0];
+  for (let index = 0; index < fila; index++) {
+    tabla.insertRow().innerHTML =
+      //averiguar porque no asigna los ids
+      // "<tr id='"+`${index}`+"'>" +
+      "<tr>" +
+      "<td>?</td>" +
+      "<td>?</td>" +
+      "<td>?</td>" +
+      "<td>?</td>" +
+      "</tr>";
+  }
 
+  //como no asigna bien los id vamos a asignarlo ahora
+  let filasTabla = document.getElementsByTagName("tr");
+  for (let index = 0; index < filasTabla.length; index++) {
+    filasTabla[index].setAttribute("id", index);
+  }
 };
+
+pintarFila = () =>{
+  
+}
+
+//llamamos a la funcion y le pasamos la dificultad
+pintarTablaSegunDificultad(dificultad);
+//pintamos los colores con los que vamos a jugar
+asignarColor();
 
 console.log(nombreJugador, dificultad, coloresAJugar);
