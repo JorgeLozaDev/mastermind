782d96c0dce84a04186bbfc872518527675649ee
jorge <j0rg3e93@gmail.com>
2023-10-13 10:22:09 +0200

    Se cambia el nombre de la clase Mybtn por myBtn para seguir los estandares, en la pagina de juego.html, ya tiene la funcionalidad, cuando pinchamos en el color pintamos la caja

--- src/js/juego.js
+++ src/js/juego.js
@@ -7,6 +7,7 @@ let currentRow = 0;
 // buscamos el id donde ira el nombre y lo colocamos en su caja
 document.getElementById("nombre").innerHTML = nombreJugador;
 
+//rellenamos las cajas con los colores que tenemos que jugar
 const asignarColor = () => {
   let cajasColores = document.getElementsByClassName("bordeCircular");
 
@@ -15,36 +16,51 @@ const asignarColor = () => {
   }
 };
 
+//segun la dificultad pintamos nuestra tabla (de momento solo pintamos el nivel facil)
 const pintarTablaSegunDificultad = (dificultad) => {
   if (dificultad == "easy") {
     pintarTabla(10);
   }
 };
 
+// function para pintar nuestra tabla dependiendo del número de filas que le pasemos
 const pintarTabla = (fila) => {
   let tabla = document.getElementById("tabla").getElementsByTagName("tbody")[0];
   for (let index = 0; index < fila; index++) {
     tabla.insertRow().innerHTML =
       //averiguar porque no asigna los ids
-      // "<tr id='"+`${index}`+"'>" +
+      // "<tr id='index'>" +
       "<tr>" +
-      "<td>?</td>" +
-      "<td>?</td>" +
-      "<td>?</td>" +
-      "<td>?</td>" +
+      "<td data-pintado=''>?</td>" +
+      "<td data-pintado=''>?</td>" +
+      "<td data-pintado=''>?</td>" +
+      "<td data-pintado=''>?</td>" +
       "</tr>";
+    // console.log(index);
   }
 
   //como no asigna bien los id vamos a asignarlo ahora
   let filasTabla = document.getElementsByTagName("tr");
   for (let index = 0; index < filasTabla.length; index++) {
-    filasTabla[index].setAttribute("id", index);
+    filasTabla[index].setAttribute("id", index + "_tabla");
   }
 };
 
-pintarFila = () =>{
-  
-}
+//con esta funcion vamos a recogemos el color al que hemos hecho click y se lo asignamos al elemento correspondiente de nuestra filas
+pintarFila = (e) => {
+  // buscamos el id de la fila actual a pintar
+  let filaPintar = document.getElementById(currentRow + "_tabla");
+
+  //recorremos sus hijos y los vamos pintando
+  for (let i = 0; i < filaPintar.childNodes.length; i++) {
+    if (filaPintar.childNodes[i].getAttribute("data-pintado") == "") {
+      filaPintar.childNodes[i].setAttribute("data-pintado", true);
+      filaPintar.childNodes[i].innerHTML = "";
+      filaPintar.childNodes[i].style.backgroundColor = e.style.backgroundColor;
+      break;
+    }
+  }
+};
 
 //llamamos a la funcion y le pasamos la dificultad
 pintarTablaSegunDificultad(dificultad);
