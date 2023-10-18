90be802ad8208672891e139d724a4ef85b01fa57
Jorge <j0rg3e93@gmail.com>
2023-10-18 18:16:07 +0200

    Se terminad de crear la estructura base de resultado.html, se cambia el tiempo de ejecucion de la animacion de fade in, se crea las funciones necesarias para la pagina de resultado y funcione como deberia

--- src/js/result.js
+++ src/js/result.js
@@ -1,2 +1,32 @@
 let resultado = sessionStorage.getItem("resultado");
-console.log(resultado)
+let jugador = sessionStorage.getItem("j1");
+
+mensajeResultado = () => {
+  let caja = document.getElementById("cajaResultado");
+  caja.innerHTML = "";
+  if (resultado == "win") {
+    caja.innerHTML =
+      "<div class='cajaResultado'> <p>Has ganado " +
+      jugador +
+      "</p> <p>¿Quieres volver a jugar?</p><div class='divBtnResult'><p class='myBtn' onclick='preLoadGame()'>SI</p><p class='myBtn' onclick='resetGame()'>NO</p></div> </div>";
+  } else {
+    caja.innerHTML =
+      "<div class='cajaResultado'> <p> " +
+      jugador +
+      " has perdido </p> <p>¿Quieres volver a jugar?</p><div class='divBtnResult'><p class='myBtn' onclick='preLoadGame()'>SI</p><p class='myBtn' onclick='resetGame()'>NO</p></div> </div>";
+  }
+};
+
+resetGame = () => {
+    // limpiamos los datos que tengamos guardados
+    sessionStorage.clear()
+    //llevamos al usuario a la pagina principal
+    window.location.href = "./index.html";
+};
+
+preLoadGame=()=>{
+    //llevamos al usario a la pagina de basic
+    window.location.href = "./juego.html";
+}
+
+mensajeResultado();
